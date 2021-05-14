c061e59bf206db7de98b90f0221d826c5b46ba04
Ashley Anderton <ashley.j.anderton@gmail.com>
2021-05-14 13:34:31 +0100

    Updated JS

--- .dev/Frontend/src/js/custom.js
+++ .dev/Frontend/src/js/custom.js
@@ -1,189 +1,152 @@
+$("html").addClass("govuk-template  flexbox no-flexboxtweener");
+$("body").addClass("govuk-template__body js-enabled");
+$(".tl-breadcrumbs li:first-child a").text("Home");
+
+
+//Prevents T Levels from wrapping
 $(document).ready(function () {
-    $(document).on("change", ".address-item", function () {
-        $("#Latitude").val("");
-        $("#Longitude").val("");
+    $('h1, h1 a, h2, h2 a, h3, h3 a:contains("T Level")').contents().each(function () {
+        if (this.nodeType == 3) {
+            $(this).parent().html(function (_, oldValue) {
+                return oldValue.replace(/T Level/g, "<span class='tl-nowrap'>$&</span>")
+            })
+        }
     });
 });
 
-// provides the matching addresses from postcode
-//if (typeof (postcodeAnywhereApiKey) !== 'undefined')
-(function ($) {
-    var searchContext = "",
-        //uri = $('form').attr('action'),
-        findAddressVal = $("#postcode-search").val();
-
-    $('#enterAddressManually').on('click',
-        function (e) {
-            e.preventDefault();
-            $('#addressManualWrapper').unbind('click');
-
-            $('#address-details').removeClass('disabled');
-            $('#AddressLine1').focus();
-        });
-
-    $('#addressManualWrapper').bind('click',
-        function () {
-            $(this).unbind('click');
-            $('#address-details').removeClass('disabled');
-            $('#AddressLine1').focus();
-        });
-
-    $("#postcode-search").keyup(function () {
-        findAddressVal = $(this).val();
+// Article voting show/hide
+$(document).ready(function () {
+    $(".tl-article--vote--button").click(function () {
+        event.preventDefault();
+        $(".tl-article--vote--buttons").addClass("tl-hidden");
+        $(".tl-article--vote--question").text("Thank you for your feedback");
     });
+});
 
-    $("#postcode-search").autocomplete({
-        source: function (request, response) {
-            $.ajax({
-                url: "//services.postcodeanywhere.co.uk/CapturePlus/Interactive/Find/v2.10/json3.ws",
-                dataType: "jsonp",
-                data: {
-                    key: postcodeAnywhereApiKey,
-                    country: 'GB',
-                    searchTerm: request.term,
-                    lastId: searchContext
-                },
-                timeout: 5000,
-                success: function (data) {
-                    //console.log(JSON.stringify(data));
 
-                    $('#postcodeServiceUnavailable').hide();
-                    $('#enterAddressManually').hide();
-                    $('#addressLoading').show();
+// Search error messaging
 
-                    $("#postcode-search").one('blur', function () {
-                        $('#enterAddressManually').show();
-                        $('#addressLoading').hide();
-                    });
+var searchbox = $(".tl-search--container input[type=search]");
 
-                    response($.map(data.Items, function (suggestion) {
-                        return {
-                            label: suggestion.Text,
-                            value: "",
-                            data: suggestion
-                        }
-                    }));
-                },
-                error: function () {
-                    $('#postcodeServiceUnavailable').show();
-                    $('#enterAddressManually').hide();
-                    $('#address-details').removeClass('disabled');
-                }
-            });
-        },
-        messages: {
-            noResults: function () {
-                return "We can't find an address matching " + findAddressVal;
-            },
-            results: function (amount) {
-                return "We've found " + amount + (amount > 1 ? " addresses" : " address") +
-                    " that match " + findAddressVal + ". Use up and down arrow keys to navigate";
-            }
-        },
-        select: function (event, ui) {
-            var item = ui.item.data;
+function showerror(message) {
+    $(".tl-search--error").removeClass("tl-hidden");
+    $(".tl-form-group").addClass("tl-form-group--error");
+    $(".tl-search #query").addClass("tl-input--error");
+    $(".tl-error--message").text(message);
+    $(".tl-input--error:visible:first").focus();
+}
 
-            if (item.Next === "Retrieve") {
-                //retrieve the address
-                retrieveAddress(item.Id);
-            } else {
-                var field = $(this);
-                searchContext = item.Id;
-
-                $('#addressLoading').show();
-                $('#enterAddressManually').hide();
-                $('#postcodeServiceUnavailable').hide();
-
-                if (searchContext === "GBR|") {
-                    window.setTimeout(function () {
-                        field.autocomplete("search", item.Text);
-                    });
-                } else {
-                    window.setTimeout(function () {
-                        field.autocomplete("search", item.Id);
-                    });
-                }
-            }
-        },
-        focus: function (event, ui) {
-            $("#addressInputWrapper").find('.ui-helper-hidden-accessible').text("To select " + ui.item.label + ", press enter");
-        },
-        autoFocus: true,
-        minLength: 1,
-        delay: 100
-    }).focus(function () {
-        searchContext = "";
-    });
-
-    function retrieveAddress(id) {
-        $('#addressLoading').show();
-        $('#enterAddressManually').hide();
-        $('#postcodeServiceUnavailable').hide();
-        $('#address-details').addClass('disabled');
-
-        $.ajax({
-            url: "//services.postcodeanywhere.co.uk/CapturePlus/Interactive/Retrieve/v2.10/json3.ws",
-            dataType: "jsonp",
-            data: {
-                key: postcodeAnywhereApiKey,
-                id: id
-            },
-            timeout: 5000,
-            success: function (data) {
-                if (data.Items.length) {
-                    $('#address-details').removeClass('disabled');
-                    $('#addressLoading').hide();
-                    $('#enterAddressManually').show();
-                    $('#addressManualWrapper').unbind('click');
-                    $("#postcode-search").val("");
-
-                    populateAddress(data.Items[0]);
-                }
-            },
-            error: function () {
-                $('#postcodeServiceUnavailable').show();
-                $('#enterAddressManually').hide();
-                $('#addressLoading').hide();
-                $('#address-details').removeClass('disabled');
-            }
-        });
-    }
-
-    function populateAddress(address) {
-        if (!$('#CompanyName').val()) {
-            $('#CompanyName').val(address.Company);
+$(document).ready(function () {
+    $(".tl-search--container").submit(function () {
+        if (!searchbox.val()) {
+            event.preventDefault();
+            showerror("You must enter a search term");
         }
-        $('#AddressLine1').val(address.Line1);
-        $('#AddressLine2').val(address.Line2);
-        $('#AddressLine3').val(address.Line3);
-        $('#AddressCity').val(address.City);
-        $('#Postcode').val(address.PostalCode);
+    });
+});
 
-        $('#ariaAddressEntered').text('Your address has been entered into the fields below.');
 
-        populateLatLng(address);
 
-        //Webtrends.multiTrack({ element: this, argsa: ["DCS.dcsuri", uri + "/findaddress", "WT.dl", "99", "WT.ti", "Settings – Find Address"] });
+/* cookie banner starts */
+//to delete cookie banner cookies ...
+//document.cookie = "seen_cookie_message_help=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+//document.cookie = "AnalyticsConsent=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+function writeCookie(key, value, days) {
+    var date = new Date();
+    days = days || 365;// Default at 365 days
+    date.setTime(+ date + (days * 86400000)); //24 * 60 * 60 * 1000
+    window.document.cookie = key + "=" + value + "; expires=" + date.toGMTString() + "; path=/";
+    return value;
+}
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+var $cookieBanner = $('#global-cookie-message-help');
+var cookieHelp = readCookie('seen_cookie_message_help');
+
+if (cookieHelp == null) {//cookie does not exist
+    var href = $cookieBanner.find('.gem-c-cookie-banner__button-settings').find('.gem-c-button').attr('href');
+    var url = window.location.href;
+}
+
+if (href == url) {
+    $cookieBanner.find('.gem-c-cookie-banner__wrapper').hide();
+} else if (cookieHelp == null) {//cookie doesn't exist
+    $cookieBanner.find('.gem-c-cookie-banner__wrapper').show();
+}
+
+$cookieBanner.find('button.gem-c-button').click(function () {
+    writeCookie('seen_cookie_message_help', 'cookie_policy', 365);
+    writeCookie('AnalyticsConsent', 'true', 365);
+
+    $cookieBanner.find('.gem-c-cookie-banner__wrapper').hide();
+    var $cookieConfirm = $cookieBanner.find('.gem-c-cookie-banner__confirmation');
+
+    $cookieConfirm.show().find('.gem-c-cookie-banner__hide-button').click(function () {
+        $cookieConfirm.hide();
+    });
+});
+/* cookie banner ends */
+
+
+/* Cookie Article, with consent starts */
+var cookieConsent = $('#select-measure-analytics');
+
+if (cookieConsent.length) {
+    $('#select-measure-analytics-btn').append('<button id="saveCookieChanges" class="govuk-button" data-module="govuk-button">Save changes</button>');
+
+    cookieConsent.append('<h3>Do you want us to measure your website use with Google Analytics?</h3><div class="govuk-form-group"><fieldset class="govuk-fieldset"><div class="govuk-radios"><div class="govuk-radios__item"><input class="govuk-radios__input" id="cookie-consent-Yes" name="allow-analytics" type="radio"><label class="govuk-label govuk-radios__label" for="cookie-consent-Yes">Yes</label></div><div class="govuk-radios__item"><input class="govuk-radios__input" id="cookie-consent-No" name="allow-analytics" type="radio"><label class="govuk-label govuk-radios__label" for="cookie-consent-No">No</label></div></div></fieldset></div>');
+
+    var cookieGoogle = readCookie('AnalyticsConsent');
+
+    if ((cookieGoogle == 'false') || (cookieGoogle == null)) {
+        $('#cookie-consent-Yes').prop("checked", false);
+        $('#cookie-consent-No').prop("checked", true);
+    } else {//not false (unset or true)
+        $('#cookie-consent-Yes').prop("checked", true);
+        $('#cookie-consent-No').prop("checked", false);
     }
 
-    function populateLatLng(address) {
-        var url = "https://api.postcodes.io/postcodes/" + address.PostalCode,
-            json;
+    $('#saveCookieChanges').on('click', function () {
+        if ($('#cookie-consent-Yes').is(':checked')) {
+            writeCookie('AnalyticsConsent', 'true', 365);
+            writeCookie('seen_cookie_message_help', 'cookie_policy', 365); //also turn off cookie banner
+        }
+        if ($('#cookie-consent-No').is(':checked')) {
+            writeCookie('AnalyticsConsent', 'false', 365); //document.cookie = "AnalyticsConsent=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //delete cookie
+            writeCookie('seen_cookie_message_help', 'cookie_policy', 365); //also turn off cookie banner
+        }
+    });
 
-        $.get(url)
-            .done(function (data) {
-                json = data;
+    /*$('#cookie-consent-Yes').change(function() { writeCookie('AnalyticsConsent','true',365); writeCookie('seen_cookie_message_help','cookie_policy',365); });
+    $('#cookie-consent-No').change(function() {writeCookie('AnalyticsConsent','false',365); writeCookie('seen_cookie_message_help','cookie_policy',365); });*/
+}
+/* Cookie Article, with consent ends */
 
-                if (json.status === 200 && json.result !== null) {
-                    $("#Latitude").val(json.result.latitude);
-                    $("#Longitude").val(json.result.longitude);
-                } else {
-                    //console.log("Call to get lat-long failed");
-                }
 
-            })
-            .fail(function () {
-                //console.log("call to get lat-long failed");
-            });
+/* Agent USER CHECK */
+function isAgent() {
+    if (HelpCenter.user.role == "agent" || HelpCenter.user.role == "manager") {
+        return true;
     }
-})(jQuery);
+    return false;
+};
+
+/* Logged in USER CHECK */
+function isLoggedIn() {
+    if (HelpCenter.user.role != "anonymous") {
+        return true;
+    }
+    return false;
+};
+
+
