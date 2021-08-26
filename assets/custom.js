175314c0aaa56b611fc5145f255a6c860fac7a26
Ashley Anderton <ashley.j.anderton@gmail.com>
2021-08-26 14:07:29 +0100

    JS fix for details

--- assets/custom.js
+++ assets/custom.js
@@ -70,8 +70,8 @@ $(document).ready(function () {
 
 $(".govuk-tabs").attr("data-module", "govuk-tabs");
 
-$(".govuk-details").attr("data-module", "govuk-details");
-$(".govuk-details__summary").click(function () {
+$(".tl-article--content .govuk-details").attr("data-module", "govuk-details");
+$(".tl-article--content .govuk-details__summary").click(function () {
     if (this.closest(".govuk-details").hasAttribute("open")) {
         $(this).closest(".govuk-details").removeAttr('open');
     }
