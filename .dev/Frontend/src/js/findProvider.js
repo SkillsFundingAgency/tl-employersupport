bb3970998e511f7bab7581286dddcebcc3ac5843
Mike Wild <wildconsultingltd@googlemail.com>
2022-06-10 14:50:19 +0100

    Add console logging for active data attribute

--- .dev/Frontend/src/js/findProvider.js
+++ .dev/Frontend/src/js/findProvider.js
@@ -446,11 +446,13 @@ function FindProvider(
             });
             $(".tl-fap--filter--clearall").removeClass("tl-hidden");
             $(".tl-fap--filter").data("active", "true");
+            console.log('set active data attribute - ' + $(".tl-fap--filter").data("active"));
         }
         else {
             $(".tl-fap--filter--selected").html('<p class="govuk-body-s govuk-!-margin-bottom-1">No filters selected</p>');
             $(".tl-fap--filter--clearall").addClass("tl-hidden");
             $(".tl-fap--filter").removeData("active");
+            console.log('removed active data attribute - ' + $(".tl-fap--filter").data("active"));
         }
     };
 
