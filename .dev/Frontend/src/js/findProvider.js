77acd6b83c10e5184b59cd11c498b8c26e1b7aab
Mike Wild <wildconsultingltd@googlemail.com>
2022-06-24 10:38:27 +0100

    Delete unused v2 code

--- .dev/Frontend/src/js/findProvider.js
+++ .dev/Frontend/src/js/findProvider.js
@@ -263,11 +263,6 @@ function FindProvider(
                 if (availableNow && !typeof availableNow.routes !== "undefined" && availableNow.routes) {
                     availableNow.routes.sort(function (x, y) { return (x.name < y.name) ? -1 : ((x.name > y.name) ? 1 : 0) });
                 }
-                //Obsolete v2 code
-                if (availableNow && !typeof availableNow.qualifications !== "undefined" && availableNow.qualifications) {
-                    availableNow.qualifications.sort(function (x, y) { return (x.name < y.name) ? -1 : ((x.name > y.name) ? 1 : 0) });
-                }
-                /////
 
                 searchResult += '</p><div class="tl-fap--courses">';
 
@@ -280,25 +275,6 @@ function FindProvider(
                             searchResult += '<div class="tl-fap--courses--box"> \
                                         <h4 class="govuk-body govuk-!-font-weight-bold">T Levels available from September  ' + deliveryYear.year + ':</h4>';
                         }
-
-                        //Obsolete v2 code
-                        if ((!typeof deliveryYear.routes === "undefined" || deliveryYear.routes === null) &&
-                            (typeof deliveryYear.qualifications !== "undefined" || deliveryYear.qualifications !== null)) {
-                            console.log('using v2 qualifications');
-                            searchResult += '<ul class="govuk-list govuk-list--bullet govuk-!-margin-bottom-1">';
-                            $.each(deliveryYear.qualifications,
-                                function (_, qualification) {
-                                    const articleLink = typeof qualificationArticleMap !== "undefined" ?
-                                        qualificationArticleMap[qualification.id] : null;
-                                    if (articleLink) {
-                                        searchResult += '<li><a target="_blank" class="govuk-link tl-fap--result-course" href="' + articleLink + '">' + qualification.name + '</a></li>';
-                                    } else {
-                                        searchResult += '<li>' + qualification.name + '</li>';
-                                    }
-                                });
-                            return;
-                        }
-                        /////
 
                         if (!typeof deliveryYear.routes === "undefined" || deliveryYear.routes === null) return;
 
@@ -464,7 +440,6 @@ function FindProvider(
             $(this).click();
             return false;
         }
-
     });
 
 
