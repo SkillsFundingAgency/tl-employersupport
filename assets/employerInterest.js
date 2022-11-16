f402a5311a61ede1b1eec1bb73a331484c7d80c6
ashleyanderton <ashley.j.anderton@gmail.com>
2022-11-16 12:02:22 +0000

    Trim

--- assets/employerInterest.js
+++ assets/employerInterest.js
@@ -6579,7 +6579,7 @@ function validateanswers(successCallback) {
         // Postcode val //
         let postcode = $("#postcode");
         var postcodereg = /^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$/i;
-        if (!postcodereg.test(postcode.val())) {
+        if (!postcodereg.test(postcode.val().trim())) {
             addError("Enter a real UK postcode, for example SW1A 1AA", postcode);
             haserror = true;
         }
