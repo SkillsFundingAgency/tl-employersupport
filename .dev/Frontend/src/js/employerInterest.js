20d7976e1986bbf088ee7bed79ceb4d1ea9542f2
ashleyanderton <ashley.j.anderton@gmail.com>
2023-02-22 13:26:08 +0000

    Uppercase postcodes

--- .dev/Frontend/src/js/employerInterest.js
+++ .dev/Frontend/src/js/employerInterest.js
@@ -469,7 +469,7 @@ function validatelocations(findProviderApiUri, findProviderAppId, findProviderAp
 
     // Postcode val //
     let postcode = $("#postcode");
-    let postcodeval = $("#postcode").val().trim();
+    let postcodeval = $("#postcode").val().toUpperCase().trim();
     var postcodereg = /^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$/i;
     if (postcodeval.length == 0) {
         addError("Enter your organisation's postcode", postcode);
